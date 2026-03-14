819b1192e122f97f4d3632b99b8f388080fb221f
Damar452 <damarh452@gmail.com>
2026-03-13 19:02:18 -0500

    [Feature] - Show user balance in header across all views

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, CurrencyPipe],
 	templateUrl: './header.component.html',
 })
 export class HeaderComponent {
 	@Input() showLogin = true;
 	@Input() userName: string | null = null;
 	@Input() userEmail: string | null = null;
+	@Input() userBalance: number | null = null;
 	@Input() showMenuButton = false;
 	@Output() menuClick = new EventEmitter<void>();
 
